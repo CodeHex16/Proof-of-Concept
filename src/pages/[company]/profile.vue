74b91eea18c252bbb946556fc773ecc656738c07
Yihao <yihao.zhuo.2001@gmail.com>
2024-12-13 03:44:36 +0100

    ✨ Add user profile page

---
+++ src/pages/[company]/profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <CompanyDrawer />
+  Profile page
+</template>
+<script lang="ts" setup>
+
+definePage({
+  meta: {
+    title: 'Profile',
+    description: 'The page for users to view their profile.',
+    requiresAuth: true,
+  },
+});
+
+</script>
